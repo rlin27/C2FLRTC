2f6a8ce7a8779e24b80bfc4f38e5c420bb0b5635
U-RUI\ruili <RuiLIN0212>
2021-12-20 14:09:19 +0800

    Use HaLRTC

--- test_performance_HaLRTC.m
+++ test_performance_HaLRTC.m
@@ -15,14 +15,14 @@ filename{8}='sailboat';
 
 %---- Fix the random seed ----
 rng(602)
-
+ss
 %---- C2F ----
 for ff=1:8  % load the test images
     myName=sprintf('test_images/%s.bmp',filename{ff});  % get the path of the selected image
     A=imread(myName);  % read the original image
     figure(1); imshow(A); title('Original Figure'); % print the orignal image
     
-    myrate=0.70:0.1:0.90;  % set the missing rate
+    myrate=0.70:0.10:0.90;  % set the missing rate
     myResult=cell(2,numel(myrate));  % build a cell to store the results
     A=double(A)/255.0;  % normalize the values of the image
     
@@ -50,22 +50,19 @@ for ff=1:8  % load the test images
                 end
             end
         end
-        figure(2);imshow(B); title(['Figure with missing ratio ', num2str(1-rate)]); % show the image with missing value
+        figure(2);imshow(B); %title(['Figure with missing ratio ', num2str(1-rate)]); % show the image with missing value
         
         tsize=[row, col, channel];  % the size of the image
-        
         % hyper parameters
-        tau = [32, 32, 1];
-        param.incR{1} = [1, 2, 4, 8, 16, 24, 32];
-        param.incR{2} = [1, 2, 4, 8, 16, 32, 64, 96, 128, 160, 192, 256];
-        param.incR{3} = [1, 2, 4, 8, 16, 24, 32];
-        param.incR{4} = [1, 2, 4, 8, 16, 32, 64, 96, 128, 160, 192, 256];
-        param.incR{5} = [1];
-        param.incR{6} = [1, 3];
-        param.delta = 1e-5;
-        sc = 255;
+        alpha = [1, 1, 1e-3];
+        alpha = alpha / sum(alpha);
+        maxIter = 500;
+        epsilon = 1e-8;
+        rho = 0.001;
         
-        fprintf('-------------- SPC -------------------\n');
+        fprintf('-------------- HaLRTC -------------------\n');
+        lambda_1=0.5;
+        lambda_2=1000;
         
         scale_num=5;
         overlap_pixel=5;  % a half
@@ -87,9 +84,9 @@ for ff=1:8  % load the test images
         [index_temp]=find(B_pad(:));
         value_temp=B_pad(index_temp);
         
-        %%%%%%%%%% STDC completion %%%%%%%%%%
-        Qms = (B_pad ~= 0);
-        Z_TRLRTV2_temp=MDT_Tucker_incR(B_pad,Qms, tau, param);
+        %%%%%%%%%% LRTC completion %%%%%%%%%%
+        Omega = (B_pad ~= 0);
+        [Z_TRLRTV2_temp,~] = HaLRTC(B_pad, Omega, alpha, rho, maxIter, epsilon);
         
         Z_LRTCTV2_completewhole{ff,iterate}=Z_TRLRTV2_temp;
 
@@ -105,7 +102,7 @@ for ff=1:8  % load the test images
         diff_r{ff,iterate}=[diff_r{ff,iterate};0];
         
         % save the result got by pure LRTC
-        store_name=sprintf('pure_SPC_restore_%s_%d.mat',filename{ff},iterate);
+        store_name=sprintf('pure_HaLRTC_restore_%s.mat',filename{ff});
         save(store_name,'Z_TRLRTV2_temp')
         
         % coarse level completion
@@ -123,22 +120,29 @@ for ff=1:8  % load the test images
             for row_wise=1:2^(i-1)
                 for col_wise=1:2^(i-1)
                     B_temp=B_pad((row_wise-1)*(patch_size(1)-2*overlap_pixel)+1:(row_wise-1)*(patch_size(1)-2*overlap_pixel)+patch_size(1),(col_wise-1)*(patch_size(2)-2*overlap_pixel)+1:(col_wise-1)*(patch_size(2)-2*overlap_pixel)+patch_size(2),:);
+                    CCCC = B_temp;
                     [index_temp]=find(B_temp(:));
                     value_temp=B_temp(index_temp);
-                     B_temp=Z_TRLRTV2_temp((row_wise-1)*(patch_size(1)-2*overlap_pixel)+1:(row_wise-1)*(patch_size(1)-2*overlap_pixel)+patch_size(1),(col_wise-1)*(patch_size(2)-2*overlap_pixel)+1:(col_wise-1)*(patch_size(2)-2*overlap_pixel)+patch_size(2),:);
+                    B_temp=Z_TRLRTV2_temp((row_wise-1)*(patch_size(1)-2*overlap_pixel)+1:(row_wise-1)*(patch_size(1)-2*overlap_pixel)+patch_size(1),(col_wise-1)*(patch_size(2)-2*overlap_pixel)+1:(col_wise-1)*(patch_size(2)-2*overlap_pixel)+patch_size(2),:);
                     sizeBtemp=prod(size(B_temp));
                     vecBtemp=randperm(sizeBtemp);
                     index_temp=[index_temp;vecBtemp(1:floor(length(vecBtemp)*0.1))'];
                     value_temp=B_temp(index_temp);
                     
                     if i<6
-                        %%%%%%%%%% STDC completion %%%%%%%%%%
-                        Qms = (B_temp ~= 0);
-                        Z_TRLRTV2_patch=MDT_Tucker_incR(B_temp, Qms, tau, param);
+                        %%%%%%%%%% LRTC completion %%%%%%%%%%
+                        alpha = [1, 1, 1e-3];
+                        alpha = alpha / sum(alpha);
+                        maxIter = 500;
+                        epsilon = 1e-8;
+                        rho = 0.001;
+                        Omega = (CCCC ~= 0);
+                        [Z_TRLRTV2_patch,~] = HaLRTC(CCCC, Omega, alpha, rho, maxIter, epsilon);
                     else
-                        %%%%%%%%%% STDC completion %%%%%%%%%%
-                        Qms = (B_temp ~= 0);
-                        Z_TRLRTV2_patch=MDT_Tucker_incR(B_temp, Qms, tau, param);
+                        %%%%%%%%%% LRTC completion %%%%%%%%%%
+                        Omega = (B_temp ~= 0);
+                        [Z_TRLRTV2_patch,~] = HaLRTC(B_temp, Omega, alpha, rho, maxIter, epsilon);
+                        
                         Z_TRLRTV2_patch=imresize(Z_TRLRTV2_patch,[size(B_temp,1),size(B_temp,2)]);
                     end
                     
@@ -181,7 +185,7 @@ for ff=1:8  % load the test images
         end
         Z_LRTCTV2_completepatch{ff,iterate}=Z_TRLRTV2_temp;
         
-        save C2F_SPC_Results rse_r r_n psnr_r diff_r Z_LRTCTV2_completepatch Z_LRTCTV2_completewhole B_padimage tmpRSE_LRTC_TV2 tmpPSNR_LRTC_TV2
+        save C2F_HaLRTC_Results rse_r r_n psnr_r diff_r Z_LRTCTV2_completepatch Z_LRTCTV2_completewhole B_padimage tmpRSE_LRTC_TV2 tmpPSNR_LRTC_TV2
         fprintf('per image per miss rate costs time:   ')
         toc
     end
